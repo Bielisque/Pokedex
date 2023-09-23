796b23bfa984cbed3298e32f5cd76bec437550fe
Liedson Bielisque <137654757+Bielisque@users.noreply.github.com>
2023-09-23 20:33:37 -0300

    Ultima Alteração; Colocando o Modal para abrir as stats.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,3 +1,4 @@
+
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -32,3 +33,10 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+// requisiçao dos status do pokemon
+pokeApi.getPokemonModalStats = (url) => {
+    return fetch(url)
+        .then((response) => response.json())
+        .then((pokemon) => pokemon);
+};
